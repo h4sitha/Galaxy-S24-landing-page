c9fcba56d04d859bf658983545a2717efc01c710
Hasitha Ekanayake <150508298+h4sitha@users.noreply.github.com>
2024-03-20 11:43:04 +0530

    Update function to change color of the icon
    Now the color of the svg will change with the
    theme. Otherwise it wouldn't be visible to the
    user.

--- script.js
+++ script.js
@@ -3,4 +3,5 @@ const themeBtn = document.querySelector('svg');
 
 themeBtn.addEventListener('click', () => {
     root.classList.toggle('dark-theme');
+    themeBtn.style.fill = (themeBtn.style.fill !== "white") ? "white" : "black";
 })
