6ba0a91c5ab90992a1a022af099d34e99eff3e2a
Hasitha Ekanayake <150508298+h4sitha@users.noreply.github.com>
2024-03-20 11:52:06 +0530

    Update the color of the logo when theme changes

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const root = document.documentElement;
 const themeBtn = document.querySelector('svg');
+const logo = document.querySelector('#samsung-logo');
 
 themeBtn.addEventListener('click', () => {
     root.classList.toggle('dark-theme');
     themeBtn.style.fill = (themeBtn.style.fill !== "white") ? "white" : "black";
+    logo.classList.toggle('logo-inverted');
 })
